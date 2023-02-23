829ea4a5170f5459425e21862cce689f0ed6bc8a
Mike Zigberman <mikezigberman@yahoo.com>
2023-02-23 21:57:33 +0100

    emailclientservice(Java Mail): the login service

--- EmailClientService/src/com/test/controller/LoginWindowController.java
+++ EmailClientService/src/com/test/controller/LoginWindowController.java
@@ -1,6 +1,8 @@
 package com.test.controller;
 
 import com.test.EmailManager;
+import com.test.controller.services.LoginService;
+import com.test.model.EmailAccount;
 import com.test.view.ViewFactory;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
@@ -25,9 +27,33 @@ public class LoginWindowController extends BaseController {
 
     @FXML
     void loginButtonAction() {
+        if(fieldsAreValid()){
+            EmailAccount emailAccount = new EmailAccount(emailAddressField.getText(), passwordField.getText());
+            LoginService loginService = new LoginService(emailAccount, emailManager);
+            EmailLoginResult emailLoginResult = loginService.login();
+
+            switch (emailLoginResult) {
+                case SUCCESS:
+                    System.out.println("login succesfull" + emailAccount);
+                    return;
+            }
+        }
         System.out.println("loginButtonAction");
         viewFactory.showMainWindow();
         Stage stage = (Stage) errorLabel.getScene().getWindow();
         viewFactory.closeStage(stage);
+    }
+
+    private boolean fieldsAreValid() {
+        if(emailAddressField.getText().isEmpty()) {
+            errorLabel.setText("Please fill email");
+            return false;
+        }
+        if(emailAddressField.getText().isEmpty()) {
+            errorLabel.setText("Please fill password");
+            return false;
+        }
+
+        return true;
     }
 }
