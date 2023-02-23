bb90360b4292476f4c7b1599bcbb4de4a8d8ab60
Mike Zigberman <mikezigberman@yahoo.com>
2023-02-23 19:29:34 +0100

    emailclientservice(Java Mail): JavaMail setup

--- EmailClientService/src/module-info.java
+++ EmailClientService/src/module-info.java
@@ -3,6 +3,8 @@ module EmailClientService{
         requires javafx.controls;
         requires javafx.graphics;
         requires javafx.web;
+        requires activation;
+        requires java.mail;
 
         opens com.test;
         opens com.test.view;
