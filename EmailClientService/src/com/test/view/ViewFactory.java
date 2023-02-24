3e7649977476e2f37e06000fd2788ca5af1134c3
Mike Zigberman <mikezigberman@yahoo.com>
2023-02-24 17:42:09 +0100

    emailclientservice(multithreading): fetching all folders

--- EmailClientService/src/com/test/view/ViewFactory.java
+++ EmailClientService/src/com/test/view/ViewFactory.java
@@ -17,10 +17,15 @@ public class ViewFactory {
 
     private EmailManager emailManager;
     private ArrayList<Stage> activeStages;
+    private boolean mainViewInitialized = false;
 
     public ViewFactory(EmailManager emailManager) {
         this.emailManager = emailManager;
         activeStages = new ArrayList<Stage>();
+    }
+
+    public boolean isMainViewInitialized(){
+        return mainViewInitialized;
     }
 
     // view options handling:
@@ -53,6 +58,7 @@ public class ViewFactory {
         System.out.println("main window called");
         BaseController controller = new MainWindowController(emailManager, this, "MainWindow.fxml");
         initializeStage(controller);
+        mainViewInitialized = true;
     }
 
     public void showOptionsWindow(){
