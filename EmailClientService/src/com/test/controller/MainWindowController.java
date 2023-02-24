aa9331de467997a779cc2253d6a61c3296cae8c0
Mike Zigberman <mikezigberman@yahoo.com>
2023-02-24 22:18:32 +0100

    emailclientservice(reading the email messages): sorting tableView elements

--- EmailClientService/src/com/test/controller/MainWindowController.java
+++ EmailClientService/src/com/test/controller/MainWindowController.java
@@ -2,6 +2,7 @@ package com.test.controller;
 import com.test.EmailManager;
 import com.test.model.EmailMessage;
 import com.test.model.EmailTreeItem;
+import com.test.model.SizeInteger;
 import com.test.view.ViewFactory;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -35,7 +36,7 @@ public class MainWindowController extends BaseController implements Initializabl
     private TableColumn<EmailMessage, String> recipientCol;
 
     @FXML
-    private TableColumn<EmailMessage, Integer> sizeCol;
+    private TableColumn<EmailMessage, SizeInteger> sizeCol;
 
     @FXML
     private TableColumn<EmailMessage, Date> dateCol;
@@ -98,7 +99,7 @@ public class MainWindowController extends BaseController implements Initializabl
         senderCol.setCellValueFactory((new PropertyValueFactory<EmailMessage, String>("sender")));
         subjectCol.setCellValueFactory((new PropertyValueFactory<EmailMessage, String>("subject")));
         recipientCol.setCellValueFactory((new PropertyValueFactory<EmailMessage, String>("recipient")));
-        sizeCol.setCellValueFactory((new PropertyValueFactory<EmailMessage, Integer>("size")));
+        sizeCol.setCellValueFactory((new PropertyValueFactory<EmailMessage, SizeInteger>("size")));
         dateCol.setCellValueFactory((new PropertyValueFactory<EmailMessage, Date>("date")));
     }
 
