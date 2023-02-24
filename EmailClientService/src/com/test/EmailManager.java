d3778a2f19587a006a044b4183adc8bfabe08ec9
Mike Zigberman <mikezigberman@yahoo.com>
2023-02-24 16:51:04 +0100

    emailclientservice(multithreading): treeItems for our folder structure

--- EmailClientService/src/com/test/EmailManager.java
+++ EmailClientService/src/com/test/EmailManager.java
@@ -1,4 +1,23 @@
 package com.test;
 
+import com.test.model.EmailAccount;
+import javafx.scene.control.TreeItem;
+
 public class EmailManager {
+
+    private TreeItem<String> foldersRoot = new TreeItem<String>("");
+
+    public TreeItem<String> getFoldersRoot(){
+        return foldersRoot;
+    }
+
+    public void addEmailAccount(EmailAccount emailAccount){
+        TreeItem<String> treeItem = new TreeItem<String>(emailAccount.getAddress());
+        treeItem.setExpanded(true);
+            treeItem.getChildren().add(new TreeItem<String>("INBOX"));
+            treeItem.getChildren().add(new TreeItem<String>("Sent"));
+            treeItem.getChildren().add(new TreeItem<String>("Folder"));
+            treeItem.getChildren().add(new TreeItem<String>("Spam"));
+        foldersRoot.getChildren().add(treeItem);
+    }
 }
--- EmailClientService/src/com/test/controller/MainWindowController.java
+++ EmailClientService/src/com/test/controller/MainWindowController.java
@@ -2,11 +2,15 @@ package com.test.controller;
 import com.test.EmailManager;
 import com.test.view.ViewFactory;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TreeView;
 import javafx.scene.web.WebView;
 
-public class MainWindowController extends BaseController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class MainWindowController extends BaseController implements Initializable {
 
     @FXML
     private WebView emailWebView;
@@ -15,7 +19,7 @@ public class MainWindowController extends BaseController {
     private TableView<?> emailsTableView;
 
     @FXML
-    private TreeView<?> emailsTreeView;
+    private TreeView<String> emailsTreeView;
 
     public MainWindowController(EmailManager emailManager, ViewFactory viewFactory, String fxmlName) {
         super(emailManager, viewFactory, fxmlName);
@@ -30,4 +34,13 @@ public class MainWindowController extends BaseController {
         viewFactory.showLoginWindow();
     }
 
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        setUpEmailsTreeView();
+    }
+
+    private void setUpEmailsTreeView() {
+        emailsTreeView.setRoot(emailManager.getFoldersRoot());
+        emailsTreeView.setShowRoot(false);
+    }
 }
