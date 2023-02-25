433b36f609862c4e4fc04f090f08b0d56ab841fc
Mike Zigberman <mikezigberman@yahoo.com>
2023-02-25 17:00:50 +0100

    emailclientservice(composing messages): compose messages service

--- EmailClientService/src/com/test/controller/services/LoginService.java
+++ EmailClientService/src/com/test/controller/services/LoginService.java
@@ -26,8 +26,8 @@ public class LoginService extends Service<EmailLoginResult> {
         };
 
         try {
-            Thread.sleep(16000);
             Session session = Session.getInstance(emailAccount.getProperties(), authenticator);
+            emailAccount.setSession(session);
             Store store = session.getStore("imaps");
             store.connect(emailAccount.getProperties().getProperty("incomingHost"),
                     emailAccount.getAddress(),
--- EmailClientService/src/com/test/model/EmailAccount.java
+++ EmailClientService/src/com/test/model/EmailAccount.java
@@ -1,5 +1,6 @@
 package com.test.model;
 
+import javax.mail.Session;
 import javax.mail.Store;
 import java.util.Properties;
 
@@ -8,6 +9,16 @@ public class EmailAccount {
     private String password;
     private Properties properties;
     private Store store;
+
+    public Session getSession() {
+        return session;
+    }
+
+    public void setSession(Session session) {
+        this.session = session;
+    }
+
+    private Session session;
 
     @Override
     public String toString() {
